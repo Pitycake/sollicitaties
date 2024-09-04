4d149d8760ed960263af5a1d81f311cf2d8ab2cf
Pitycake <berndtwoest@hotmail.com>
2024-09-04 20:57:11 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Add a simple route to test if the server is working
+app.get('/test', (req, res) => {
+    res.send('The server is working!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
